0c50393ad16067a6d3e2320bc6dc2d9b35c22dc1
aljhonhernandez326-beep <aljhonhernandez326@gmail.com>
2025-09-16 17:29:55 +0800

    message mo dito

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '93bffacd0cff4ae42746263d8531f06773d8dfc8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '93bffacd0cff4ae42746263d8531f06773d8dfc8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
